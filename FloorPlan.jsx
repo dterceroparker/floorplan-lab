6c2343e9c336803806ccae01397cd8b7c26b5ca6
DoribelTerceroParker <dtercerocunystart@gmail.com>
2024-04-29 17:03:42 -0400

    FloorPlan, Kitchen and livingRoom complete

---
+++ FloorPlan.jsx
@@ -0,0 +1,14 @@
+import Kitchen from "./src/Kitchen";
+import LivingRoom from "./src/LivingRoom";
+
+const FloorPlan = (props) => {
+  return ( 
+    <div>
+      <Kitchen />
+      <LivingRoom />
+      
+    </div>
+  );
+}
+
+export default FloorPlan
